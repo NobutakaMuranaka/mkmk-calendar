9cc4f878857fcc059f21a5839562cfc36499d500
NobutakaMuranaka <chanayataka2324@gmail.com>
2021-09-09 13:34:55 +0000

    endのエラーが解決できない

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'users/index'
   root to: 'blogs#index'
   resources :blogs
-
+  
+  get :signup,       to: 'users#new'
+  resources :users
+  
 end
